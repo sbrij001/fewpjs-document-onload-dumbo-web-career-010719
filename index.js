8a658d7df2456740abb9a4a9b4fe93ac87f09fbd
sbrij001 <sbrij001@yahoo.com>
2019-09-30 17:38:17 -0400

    DOMcontent loaded exercise

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Your code goes here
 function domContentLoaded(){
-    document.addEventListener("DOMContentLoaded", updateDOM,false);
-};
-
+  document.addEventListener('DOMContentLoaded', updateDOM())
+}
 
 function updateDOM(event){
-  document.querySelector("#text").innerHTML = "This is really cool!"
-};
+  let text = document.querySelector('#text');
+  text.innerHTML='This is really cool!';
+  text.innerHTML='JavaScript is so cool. It lets me add text to my page programmatically.';
+}
